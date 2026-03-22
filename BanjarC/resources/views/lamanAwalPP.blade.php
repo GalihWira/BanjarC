2744238330ef4a75cd151c12d859cae598365d70
Kariza <karizatrisnaw@gmail.com>
2026-03-22 13:14:14 +0800

    feat: Menambah halaman awal Pengaduan & Peminjaman

---
+++ BanjarC/resources/views/lamanAwalPP.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Belgrano&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Sora:wght@100..800&display=swap" rel="stylesheet">
+    @vite(['resources/css/app.css'])
+    <title>Document</title>
+</head>
+<body class="bg-primary-50">
+    <svg xmlns="http://www.w3.org/2000/svg" width="86" height="88" viewBox="0 0 86 88" fill="none">
+        <g filter="url(#filter0_d_577_3478)">
+            <path d="M0 0H80V44.4444C80 64.0812 64.0812 80 44.4444 80H0V0Z" fill="white"/>
+        </g>
+        <path d="M25.0068 12.8887C25.3872 12.8887 25.7526 13.0397 26.0215 13.3086C26.2902 13.5774 26.4413 13.9421 26.4414 14.3223V23.2285C26.4414 23.6088 26.2904 23.9742 26.0215 24.2432C25.7526 24.5121 25.3872 24.6631 25.0068 24.6631H16.1006C15.7205 24.663 15.3558 24.5119 15.0869 24.2432C14.818 23.9742 14.667 23.6088 14.667 23.2285V14.3223C14.6671 13.9421 14.8181 13.5774 15.0869 13.3086C15.3558 13.0397 15.7204 12.8888 16.1006 12.8887H25.0068ZM17.5352 21.7949H23.5732V15.7568H17.5352V21.7949Z" fill="#004481" stroke="#004481" stroke-width="0.888889"/>
+        <path d="M25.0066 31.2949C25.3869 31.2949 25.7523 31.4459 26.0213 31.7148C26.29 31.9837 26.4411 32.3484 26.4412 32.7285V41.6348C26.4412 42.0151 26.2902 42.3805 26.0213 42.6494C25.7523 42.9183 25.3869 43.0693 25.0066 43.0693H16.1004C15.7202 43.0692 15.3555 42.9182 15.0867 42.6494C14.8178 42.3805 14.6668 42.0151 14.6668 41.6348V32.7285C14.6669 32.3483 14.8178 31.9837 15.0867 31.7148C15.3556 31.446 15.7202 31.295 16.1004 31.2949H25.0066ZM17.5349 40.2012H23.573V34.1631H17.5349V40.2012Z" fill="#004481" stroke="#004481" stroke-width="0.888889"/>
+        <path d="M25.0066 49.7012C25.3869 49.7012 25.7523 49.8522 26.0213 50.1211C26.29 50.3899 26.4411 50.7546 26.4412 51.1348V60.041C26.4412 60.4213 26.2902 60.7867 26.0213 61.0557C25.7523 61.3246 25.3869 61.4756 25.0066 61.4756H16.1004C15.7202 61.4755 15.3555 61.3244 15.0867 61.0557C14.8178 60.7867 14.6668 60.4213 14.6668 60.041V51.1348C14.6669 50.7546 14.8178 50.3899 15.0867 50.1211C15.3556 49.8522 15.7202 49.7013 16.1004 49.7012H25.0066ZM17.5349 58.6074H23.573V52.5693H17.5349V58.6074Z" fill="#004481" stroke="#004481" stroke-width="0.888889"/>
+        <path d="M64.7879 17.3916C65.1682 17.3916 65.5336 17.5426 65.8025 17.8115C66.0713 18.0804 66.2224 18.4451 66.2224 18.8252C66.2224 19.2055 66.0715 19.5709 65.8025 19.8398C65.5336 20.1088 65.1682 20.2598 64.7879 20.2598H32.1316C31.7515 20.2597 31.3868 20.1086 31.118 19.8398C30.849 19.5709 30.698 19.2055 30.698 18.8252C30.6981 18.445 30.8491 18.0804 31.118 17.8115C31.3868 17.5427 31.7514 17.3917 32.1316 17.3916H64.7879Z" fill="#004481" stroke="#004481" stroke-width="0.888889"/>
+        <path d="M64.7879 35.748C65.1682 35.748 65.5336 35.899 65.8025 36.168C66.0713 36.4368 66.2224 36.8015 66.2224 37.1816C66.2224 37.562 66.0715 37.9274 65.8025 38.1963C65.5336 38.4652 65.1682 38.6162 64.7879 38.6162H32.1316C31.7515 38.6161 31.3868 38.465 31.118 38.1963C30.849 37.9274 30.698 37.562 30.698 37.1816C30.6981 36.8014 30.8491 36.4368 31.118 36.168C31.3868 35.8991 31.7514 35.7481 32.1316 35.748H64.7879Z" fill="#004481" stroke="#004481" stroke-width="0.888889"/>
+        <path d="M64.7879 54.1045C65.1682 54.1045 65.5336 54.2555 65.8025 54.5244C66.0713 54.7933 66.2224 55.158 66.2224 55.5381C66.2224 55.9184 66.0715 56.2838 65.8025 56.5527C65.5336 56.8217 65.1682 56.9727 64.7879 56.9727H32.1316C31.7515 56.9726 31.3868 56.8215 31.118 56.5527C30.849 56.2838 30.698 55.9184 30.698 55.5381C30.6981 55.1579 30.8491 54.7933 31.118 54.5244C31.3868 54.2556 31.7514 54.1046 32.1316 54.1045H64.7879Z" fill="#004481" stroke="#004481" stroke-width="0.888889"/>
+        <defs>
+            <filter id="filter0_d_577_3478" x="-1.77778" y="0" width="87.1111" height="87.1111" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+            <feOffset dx="1.77778" dy="3.55556"/>
+            <feGaussianBlur stdDeviation="1.77778"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_577_3478"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_577_3478" result="shape"/>
+            </filter>
+        </defs>
+    </svg>
+    <main class="flex items-center pb-[56px] px-20">
+        <section class="w-[1282px] h-[641px] bg-white font-sora rounded-[14px] drop-shadow-[7px_8px_4.1px_rgba(0,0,0,0.5)] flex flex-col items-center pt-[39px] -mt-2">
+            <h1 class="text-[64px] font-bold text-primary-200 px-12 mb-[21px]">Pengaduan & Peminjaman</h1>
+            <div class="rounded-t-[13px] w-280 h-15 bg-second-50 border border-black">
+                <h2 class="px-21 text-white font-bold text-[24px] pt-2.5 pl-6.5 ">Layanan Pengaduan</h2>
+                <div class="border border-black bg-white w-280 h-33 flex flex-col items-center">
+                    <p class="text-black text-[20px] pt-[12.64px] pl-6.5">Banjar C membuka layanan pengaduan seputar kendala ataupun masalah yang terjadi di banjar. Layanan ini diharapkan dapat mempermudah masyarakat  untuk menyampaikan aspirasi ataupun keluhannya. </p>
+                    <button class="mt-[8px] text-white bg-primary-300 border border-[#081E31] rounded-lg px-[43px] py-[4px]">Buat Pengaduan</button>
+                </div>
+            </div>
+            <div class="rounded-t-[13px] w-280 h-15 bg-second-50 border border-black mt-[154px]">
+                <h2 class="px-21 text-white font-bold text-[24px] pt-2.5 pl-6.5 ">Layanan Peminjaman</h2>
+                <div class="border border-black bg-white w-280 h-33 flex flex-col items-center">
+                    <p class="text-black text-[20px] pt-[12.64px] pl-6.5">Banjar C membuka layanan peminjaman tempat dan inventaris yang dimiliki oleh banjar secara digital. Layanan ini diharapkan dapat mempermudah masyarakat yang memerlukan tempat ataupun inventaris.</p>
+                    <button class="mt-[8px] text-white bg-primary-300 border border-[#081E31] rounded-lg px-[43px] py-[4px]">Ajukan Peminjaman</button>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
