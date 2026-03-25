a699b4afd0e4aadf326bb948cd3a4787d46b6d3b
Kariza <karizatrisnaw@gmail.com>
2026-03-25 17:55:34 +0800

    feat: Page daftar Tagihan

--- BanjarC/routes/web.php
+++ BanjarC/routes/web.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get ('/', [HomeController::class, 'home']);
+//Route::get ('/', [HomeController::class, 'home']);
+//Route::get ('/', [HomeController::class, 'lamanAwalPP']);
+Route::get ('/', [HomeController::class, 'daftarTagihan']);
